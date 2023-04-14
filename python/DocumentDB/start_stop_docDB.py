4c80fe008d76bbd8442053b4b57a396fd93ef93f
antonionovaesjr <antonionovaesjr@gmail.com>
2023-04-14 16:49:14 -0300

    Update start_stop_docDB.py

--- python/DocumentDB/start_stop_docDB.py
+++ python/DocumentDB/start_stop_docDB.py
@@ -6,11 +6,10 @@ import boto3
 
 def lambda_handler(event, context):
 
-
     client = boto3.client('docdb')
-
-
+    
     try:
+        print(event)
         if os.environ.get('DOCDB_NAME') is None:
             documentdb_name = event['DOCDB_NAME']
         else:
@@ -24,37 +23,37 @@ def lambda_handler(event, context):
         print("não foi possível obter o valor da variável DOCDB_ACTION ou DOCDB_NAME")
         sys.exit(1)
 
-
-
     if action_desired == "start":
-        try: 
-            response = client.start_db_cluster(
-            DBClusterIdentifier=documentdb_name
-            )
-            print(response)
-            print("######################")
-            print("######################")
-            print("#########INFO#########")
-            print("O Cluster {} foi iniciado".format(documentdb_name))
-        except Exception as e:
-            print("não foi possível iniciar o cluster {}".format(documentdb_name))
-            print(e)
-            sys.exit(1)
+        
+            for docdb in documentdb_name:
+                
+                try: 
+                    response = client.start_db_cluster(
+                    DBClusterIdentifier=docdb
+                    )
+                    print(response)
+                    print("######################")
+                    print("######################")
+                    print("#########INFO#########")
+                    print("O Cluster {} foi iniciado".format(docdb))
+                except Exception as e:
+                    print("não foi possível iniciar o cluster {}".format(docdb))
+                    print(e)
         
     elif action_desired == "stop":
-        try:
-            response = client.stop_db_cluster(
-            DBClusterIdentifier=documentdb_name
-            )
-            print(response)
-            print("######################")
-            print("######################")
-            print("#########INFO#########")
-            print("O Cluster {} foi parado".format(documentdb_name))
-        except Exception as e:
-            print("não foi possível para o cluster {}".format(documentdb_name))
-            print(e)
-            sys.exit(1)
+        for docdb in documentdb_name:        
+            try:
+                response = client.stop_db_cluster(
+                DBClusterIdentifier=docdb
+                )
+                print(response)
+                print("######################")
+                print("######################")
+                print("#########INFO#########")
+                print("O Cluster {} foi parado".format(docdb))
+            except Exception as e:
+                print("não foi possível para o cluster {}".format(docdb))
+                print(e)
             
     elif action_desired is None:
         print("Nenhuma ação possível foi especificada, valores possíveis: start ou stop")
@@ -64,6 +63,10 @@ def lambda_handler(event, context):
 
 
 if __name__ == "__main__":
-    #event = {"DOCDB_NAME":"labdevops-test","DOCDB_ACTION":"start"}
-    event = ""
+    #event = {"DOCDB_NAME":"labdevops-testaaaaaaa","DOCDB_ACTION":"start"}
+    event = {
+        "DOCDB_NAME":
+            ["labdevops-testaaaaaaa","labdevops-testbbbbbb","labdevops-testcccccc"]
+             ,"DOCDB_ACTION":"stop"
+            }
     lambda_handler(event,"")
